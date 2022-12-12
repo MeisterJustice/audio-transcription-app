e57bf434f695782ffbd89e0a67bc6643a85a234b
meisterjustice <eziefulejustice@gmail.com>
2022-12-12 15:16:51 +0100

    chore: design box

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <div className="home">
-      <div>Hello</div>
+      <div className="home__box container">
+        <div className="home__main">
+          <h1 className="home__main h1">Embrace Yourself</h1>
+          <div className="home__main author">by Author Unknown</div>
+        </div>
+        <div className="home__footer"></div>
+      </div>
     </div>
   );
 }
